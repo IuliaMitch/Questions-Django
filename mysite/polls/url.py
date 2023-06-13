51e785e8131d1e1b4e615baec57b420bcd705921
iulia <noipasz@gmail.com>
2023-06-13 17:58:22 -0300

    feat(html): creating classes for stylish

--- mysite/polls/url.py
+++ mysite/polls/url.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'polls'
+
 urlpatterns =[
     path('', views.Index, name='index'),
-    path('/<int:question_id>/vote/', views.Vote, name='vote'),
-    path('/<int:question_id>/results/', views.Results, name='results')
+    path('<int:question_id>/vote/', views.Vote, name='vote'),
+    path('<int:question_id>/results/', views.Results, name='results')
 ]
