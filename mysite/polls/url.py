8820911ab60d27ea9466d0855714328ba8c87e02
iulia <noipasz@gmail.com>
2023-06-13 17:39:32 -0300

    fix(url): remove unused path

--- mysite/polls/url.py
+++ mysite/polls/url.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns =[
     path('', views.Index, name='index'),
     path('/<int:question_id>/vote/', views.Vote, name='vote'),
-    path('/<int:question_id>/', views.Detail, name='detail'),
     path('/<int:question_id>/results/', views.Results, name='results')
 ]
