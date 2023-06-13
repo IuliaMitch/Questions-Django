1f83a0483e77128d47e6e89ce66e420441f345f9
iulia <noipasz@gmail.com>
2023-06-13 17:09:11 -0300

    feat(polls, db): made a migration and added Question model to admin manager

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,13 +10,12 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
         return self.pub_date >= (timezone.now() - datatime.timedelta(days=1))
-    
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
-    def __str__(self) -> str:
+    def __str__(self):
         return self.choice_text
