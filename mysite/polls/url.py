2360382137c00bf12daedf7504738ca571b1918a
iulia <noipasz@gmail.com>
2023-06-13 17:35:58 -0300

    feat(url): adding other paths

--- mysite/polls/url.py
+++ mysite/polls/url.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.Index, name='index')
+    path('', views.Index, name='index'),
+    path('/<int:question_id>/vote/', views.Vote, name='vote'),
+    path('/<int:question_id>/', views.Detail, name='detail'),
+    path('/<int:question_id>/results/', views.Results, name='results')
 ]
